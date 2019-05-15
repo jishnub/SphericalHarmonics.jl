4e6dfb529f61d4ecc6ce3035d6187fe1e7c48c3a
Christoph Ortner <c.ortner@warwick.ac.uk>
2019-05-15 17:24:29 +0100

    start transition to v1.x

--- src/SphericalHarmonics.jl
+++ src/SphericalHarmonics.jl
@@ -1,8 +1,7 @@
-__precompile__(true)
+
 
 module SphericalHarmonics
 
-export *
 
 """
 	sizeP(maxDegree)
@@ -15,7 +14,7 @@ sizeP(maxDegree) = div((maxDegree + 1) * (maxDegree + 2), 2)
 """
 	sizeY(maxDegree)
 
-Return the size of the set of real spherical harmonics ``Y_{l,m}(θ,φ)`` of 
+Return the size of the set of real spherical harmonics ``Y_{l,m}(θ,φ)`` of
 degree less than or equal to the given maximum degree
 """
 sizeY(maxDegree) = (maxDegree +1 ) * (maxDegree + 1)
@@ -34,20 +33,22 @@ index_p(l,m) = m + div(l*(l+1), 2) + 1
 
 Return the index into a flat array of real spherical harmonics ``Y_{l,m}``
 for the given indices ``(l,m)``.
-``Y_{l,m}`` are stored in l-major order i.e. 
+``Y_{l,m}`` are stored in l-major order i.e.
 [Y(0,0), [Y(1,-1), Y(1,0), Y(1,1), Y(2,-2), ...]
 """
 index_y(l,m) = m + l + (l*l) + 1
 
-type ALPCoefficients
+"""
+TODO: documentation
+"""
+struct ALPCoefficients
 	A::Array{Float64}
 	B::Array{Float64}
-	function ALPCoefficients(maxDegree::Int)
-		A = Array{Float64}(sizeP(maxDegree))
-		B = Array{Float64}(sizeP(maxDegree))
-		new(A, B)
-	end
 end
+
+ALPCoefficients(maxDegree::Int) =
+	ALPCoefficients( Array{Float64}(undef, sizeP(maxDegree)),
+						  Array{Float64}(undef, sizeP(maxDegree)) )
 
 """
 	compute_coefficients(L)
@@ -71,12 +72,10 @@ end
 """
 	compute_coefficients(L)
 
-Create an array large enough to store an entire set of Associated Legendre 
-Polynomials ``P_l^m(x)`` of maximum degree L. 
+Create an array large enough to store an entire set of Associated Legendre
+Polynomials ``P_l^m(x)`` of maximum degree L.
 """
-function allocate_p(L::Int)
-	P = Array{Float64}(sizeP(L))
-end
+allocate_p(L::Int) = Array{Float64}(undef, sizeP(L))
 
 """
 	compute_p(L, x, coeff, P)
@@ -84,8 +83,8 @@ end
 Compute an entire set of Associated Legendre Polynomials ``P_l^m(x)``
 using the given coefficients, and store in the array P.
 """
-function compute_p(L::Int, x::Float64, coeff::ALPCoefficients, 
-	P::Array{Float64,1})
+function compute_p(L::Int, x::Float64, coeff::ALPCoefficients,
+					    P::Array{Float64,1})
 	@assert length(coeff.A) >= sizeP(L)
 	@assert length(coeff.B) >= sizeP(L)
 	@assert length(P) >= sizeP(L)
@@ -95,9 +94,9 @@ function compute_p(L::Int, x::Float64, coeff::ALPCoefficients,
 	P[index_p(0, 0)] = temp
 
 	if (L > 0)
-		const SQRT3 = 1.7320508075688772935
+		SQRT3 = 1.7320508075688772935
 		P[index_p(1, 0)] = x * SQRT3 * temp
-		const SQRT3DIV2 = -1.2247448713915890491
+		SQRT3DIV2 = -1.2247448713915890491
 		temp = SQRT3DIV2 * sintheta * temp
 		P[index_p(1, 1)] = temp
 
@@ -121,25 +120,25 @@ Compute an entire set of Associated Legendre Polynomials ``P_l^m(x)`` where
 ``0 ≤ l ≤ L`` and ``0 ≤ m ≤ l``. Assumes ``|x| ≤ 1``.
 """
 function compute_p(L::Int, x::Float64)
-	P = Array{Float64}(sizeP(L))
+	P = Array{Float64}(undef, sizeP(L))
 	coeff = compute_coefficients(L)
-	compute_p(L, x, coeff, P)
+	compute_p(L, x, coeff, P)   # TODO: should have ! in the name
 	return P
 end
 
 """
 	compute_y(L, P, φ, Y)
 
-Compute an entire set of real spherical harmonics ``Y_{l,m}(θ,φ)`` 
+Compute an entire set of real spherical harmonics ``Y_{l,m}(θ,φ)``
 using the given Associated Legendre Polynomials ``P_l^m(cos θ)``
 and store in array Y
 """
 function compute_y(L::Int, P::Array{Float64,1}, phi::Float64,
-	Y::Array{Float64,1})
+						 Y::Array{Float64,1})
 	@assert length(P) >= sizeP(L)
 	@assert length(Y) >= sizeY(L)
 
-	const SQRT2 = 1.41421356237309504880
+	SQRT2 = 1.41421356237309504880
 	for l in 0:L
 		Y[index_y(l, 0)] = P[index_p(l, 0)] * 0.5 * SQRT2
 	end
@@ -160,16 +159,17 @@ function compute_y(L::Int, P::Array{Float64,1}, phi::Float64,
 			Y[index_y(l, m)] = P[index_p(l, m)] * c
 		end
 	end
-end
+	return Y
+end   # TODO: mutates Y => should have ! in the name
 
 """
 	compute_y(L, x, φ)
 
-Compute an entire set of real spherical harmonics ``Y_{l,m}(θ,φ)`` for 
+Compute an entire set of real spherical harmonics ``Y_{l,m}(θ,φ)`` for
 ``x = cos θ`` where ``0 ≤ l ≤ L`` and ``-l ≤ m ≤ l``.
 """
 function compute_y(L::Int, x::Float64, phi::Float64)
-	P = Array{Float64}(sizeP(L))
+	P = Array{Float64}(undef, sizeP(L))
 	coeff = compute_coefficients(L)
 	compute_p(L, x, coeff, P)
 	Y = Array{Float64}(sizeY(L))
