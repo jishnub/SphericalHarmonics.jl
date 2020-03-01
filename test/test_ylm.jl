8ec813f3880d97dfbee842e792eeb9d48bdf8b2b
Jishnu Bhattacharya <jishnuonline@gmail.com>
2020-03-01 11:05:31 +0400

    added special methods for multiples of pi/2

--- test/test_ylm.jl
+++ test/test_ylm.jl
@@ -1,4 +1,3 @@
-
 @testset "Ylm" begin
 
 function explicit_shs(θ, φ)
@@ -52,6 +51,94 @@ end # @testset
       @testset "Ylm" begin
          @test compute_y(10,-1,0) ≈ compute_y(10,SouthPole())
          @test compute_y(10,-1,0) ≈ compute_y(10,SouthPole(),π/2)
+      end
+   end
+end
+
+@testset "multiples of π/2" begin
+   @testset "integer multiplication" begin
+      @testset "Piby2" begin
+         @test 0*Piby2() == TwoPi()
+         @test 1*Piby2() == Piby2()
+         @test 2*Piby2() == Pi()
+         @test 3*Piby2() == ThreePiby2()
+         @test 4*Piby2() == TwoPi()
+         @test 5*Piby2() == Piby2()
+      end
+      @testset "Pi" begin
+         @test 0*Pi() == TwoPi()
+         @test 1*Pi() == Pi()
+         @test 2*Pi() == TwoPi()
+         @test 3*Pi() == Pi()
+         @test 4*Pi() == TwoPi()
+         @test 5*Pi() == Pi()
+      end
+      @testset "ThreePiby2" begin
+         @test 0*ThreePiby2() == TwoPi()
+         @test 1*ThreePiby2() == ThreePiby2()
+         @test 2*ThreePiby2() == Pi()
+         @test 3*ThreePiby2() == Piby2()
+         @test 4*ThreePiby2() == TwoPi()
+         @test 5*ThreePiby2() == ThreePiby2()
+      end
+      @testset "TwoPi" begin
+         @test 0*TwoPi() == TwoPi()
+         @test 1*TwoPi() == TwoPi()
+         @test 2*TwoPi() == TwoPi()
+         @test 3*TwoPi() == TwoPi()
+         @test 4*TwoPi() == TwoPi()
+         @test 5*TwoPi() == TwoPi()
+      end
+   end
+   @testset "float multiplication" begin
+      @testset "Piby2" begin
+         for n in -5:5
+            @test float(n)*Piby2() == float(n)*π/2
+         end
+      end
+      @testset "Pi" begin
+         for n in -5:5
+            @test float(n)*Pi() == float(n)*π
+         end
+      end
+      @testset "ThreePiby2" begin
+         for n in -5:5
+            @test float(n)*ThreePiby2() == float(n)*3π/2
+         end
+      end
+      @testset "TwoPi" begin
+         for n in -5:5
+            @test float(n)*TwoPi() == float(n)*2π
+         end
+      end
+   end
+   @testset "cis" begin
+      @test cis(Piby2()) ≈ cis(π/2)
+      @test cis(Pi()) ≈ cis(π)
+      @test cis(ThreePiby2()) ≈ cis(3π/2)
+      @test cis(TwoPi()) ≈ cis(2π)
+      @test cis(TwoPi()) ≈ cis(0)
+   end
+   @testset "Y" begin
+      @testset "cosθ specified" begin
+         @test compute_y(10,0.5,Piby2()) ≈ compute_y(10,0.5,π/2)
+         @test compute_y(10,0.5,Pi()) ≈ compute_y(10,0.5,π)
+         @test compute_y(10,0.5,ThreePiby2()) ≈ compute_y(10,0.5,3π/2)
+         @test compute_y(10,0.5,TwoPi()) ≈ compute_y(10,0.5,2π)
+      end
+      @testset "special points" begin
+         @testset "north pole" begin
+            @test compute_y(10,NorthPole(),Piby2()) ≈ compute_y(10,NorthPole(),π/2)
+            @test compute_y(10,NorthPole(),Pi()) ≈ compute_y(10,NorthPole(),π)
+            @test compute_y(10,NorthPole(),ThreePiby2()) ≈ compute_y(10,NorthPole(),3π/2)
+            @test compute_y(10,NorthPole(),TwoPi()) ≈ compute_y(10,NorthPole(),2π)
+         end
+         @testset "south pole" begin
+            @test compute_y(10,SouthPole(),Piby2()) ≈ compute_y(10,SouthPole(),π/2)
+            @test compute_y(10,SouthPole(),Pi()) ≈ compute_y(10,SouthPole(),π)
+            @test compute_y(10,SouthPole(),ThreePiby2()) ≈ compute_y(10,SouthPole(),3π/2)
+            @test compute_y(10,SouthPole(),TwoPi()) ≈ compute_y(10,SouthPole(),2π)
+         end
       end
    end
 end
